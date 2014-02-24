c9322f94cf41be5e90c1dab7c6cd033207d851f2
Raekye <adrian@creatifcubed.com>
2014-02-24 13:31:27 -0500

    changed scala build to manual compile over gradle

--- scala/run.sh
+++ scala/run.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
 
+echo "Cleaning..."
+rm -rf build
+echo
+
+echo "Preparing..."
+mkdir -p build/classes/main
+
 echo "Building..."
-gradle build
+#gradle build
+scalac -sourcepath src/main/scala -d build/classes/main src/main/scala/com/creatifcubed/clavier/App.scala
 echo
 
 echo "Running..."
-java -jar build/libs/scala.jar
+#java -jar build/libs/scala.jar
+scala -classpath build/classes/main com.creatifcubed.clavier.App
 
 echo
 echo "Done."
