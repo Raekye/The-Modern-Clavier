a1120686ee3627770a15a3c50c94efce6e2f155e
Raekye <adrian@creatifcubed.com>
2014-04-07 20:00:24 -0400

    scalable unique id generation and mapping ids to a changing set of operating/worker-nodes

--- scala/run.sh
+++ scala/run.sh
@@ -1,20 +1,23 @@
 #!/bin/sh
 
+MAIN_CLASS="App"
+if [[ ! -z "$1" ]]; then
+	MAIN_CLASS="$1"
+fi
+
 echo "Cleaning..."
 rm -rf build
-echo
 
 echo "Preparing..."
 mkdir -p build/classes/main
 
 echo "Building..."
 #gradle build
-scalac -sourcepath src/main/scala -d build/classes/main src/main/scala/com/creatifcubed/clavier/App.scala
-echo
+scalac -sourcepath src/main/scala -d build/classes/main "src/main/scala/com/creatifcubed/clavier/$MAIN_CLASS.scala"
 
 echo "Running..."
 #java -jar build/libs/scala.jar
-scala -classpath build/classes/main com.creatifcubed.clavier.App
+scala -classpath build/classes/main "com.creatifcubed.clavier.$MAIN_CLASS"
 
 echo
 echo "Done."
